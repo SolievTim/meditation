4fe61058c445e13b33ff9341dc73f0942fe859e3
aboba <vika14.00@list.ru>
2022-02-14 11:55:54 +0300

    registration next

---
+++ soliev_meditation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  soliev_meditation
+//
+//  Created by Student on 13.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
